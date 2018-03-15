b4dfd203a9f264c92a6d4c809daffcb14371d301
Cryptomental <cryptomental.com@gmail.com>
2018-03-15 15:02:12 +0100

    reducers: Add prefunding reducer.
    The reducer brings prefunding value to props.
    
    Issue: #37

---
+++ src/reducers/prefunding.js
@@ -0,0 +1,37 @@
+export default function reducer(
+  state = {
+    prefunding: null,
+    loading: false,
+    error: null,
+  },
+  action,
+) {
+  const actionType = 'CALCULATE_PREFUNDING';
+  switch (action.type) {
+    case `${actionType}_PENDING`:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case `${actionType}_FULFILLED`:
+      return {
+        ...state,
+        loading: false,
+        prefunding: action.payload,
+        error: null,
+      };
+
+    case `${actionType}_REJECTED`:
+      return {
+        ...state,
+        loading: false,
+        prefunding: null,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
