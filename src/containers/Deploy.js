ae79445353febb6a7d28ec8691b942267d8da12e
Cryptomental <cryptomental.com@gmail.com>
2018-03-17 16:05:00 +0100

    containers/Deploy: Add onHandlePrefunding and prefunding store values.
    Issue: #37

--- src/containers/Deploy.js
+++ src/containers/Deploy.js
@@ -1,17 +1,20 @@
 import { connect } from 'react-redux';
 
 import Contracts from '../Contracts.js';
-import { deployContract } from '../actions/deploy';
+import { deployContract, handlePreFunding } from '../actions/deploy';
 import DeployContractForm from '../components/DeployContract/DeployContractForm';
 import store from '../store';
 
 const mapStateToProps = state => {
   const { loading, error, contract } = state.deploy;
+  const { prefunding, prefunding_loading } = state.prefunding;
 
   return {
     loading,
     error,
-    contract
+    contract,
+    prefunding,
+    prefunding_loading,
   };
 };
 
@@ -22,7 +25,14 @@ const mapDispatchToProps = dispatch => {
         web3: store.getState().web3.web3Instance,
         contractSpecs
       }, Contracts));
-    }
+    },
+    onHandlePreFunding: (changedValues, contractData) => {
+      dispatch(handlePreFunding({
+        web3: store.getState().web3.web3Instance,
+        changedValues,
+        contractData,
+      }, Contracts));
+    },
   };
 };
 
